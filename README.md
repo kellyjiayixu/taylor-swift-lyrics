c32fdb275a296a0d411c43622369062e3d9d72e5
kellyjiayixu <kellyjiayixu@gmail.com>
2023-04-12 04:26:20 -0500

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # taylor-swift-lyrics
+
+## Download and Install `Node.js`
+
++ Download
+
+Go to the [Node.js official website] (https://nodejs.org/en) and download the Windows version.
+
++ Install
+
+Check if the installation is successful: 
+```
+C:\Users\raysp>node -v
+v18.15.0
+
+C:\Users\raysp>npm -v
+9.5.0
+```
+
+## Taylor Swift Lyrics Application
+
+
+
--- server.js
+++ server.js
@@ -18,9 +18,9 @@ fs.readFile(__dirname + "/taylor_swift_lyrics.csv", async (err, data) => {
   console.log("lyrics lines: " + taylor.length);
 });
 
-const min = 0; //最小值
-const max = 4849; //最大值
-const range = max - min; //取值范围差
+const min = 0; 
+const max = 4849;
+const range = max - min;
 
 var index;
 
@@ -28,37 +28,28 @@ var corsOptions = {
   origin: "*",
 };
 
-// app.get("/getLyric", function (req, res) {
-//   var random = Math.random(); //小于1的随机数
-//   index = min + Math.round(random * range); //最小数加随机数*范围差
-//   var s = "";
-//   console.log(index + " : " + taylor[index].lyric);
-//   var s = JSON.stringify(taylor[index]);
-//   res.end(s);
-// });
-
 app.get("/getLyric", function (req, res) {
-  var random = Math.random(); //小于1的随机数
-  index = min + Math.round(random * range); //最小数加随机数*范围差
+  var random = Math.random(); // random number
+  index = min + Math.round(random * range); // random number * range
   var s = "";
   console.log(index + " : " + taylor[index].lyric);
 
-  var year = taylor[index].year;
-  var track_title = taylor[index].track_title;
-  var i = index - taylor[index].line + 1;
-  console.assert(
-    taylor[i].track_title == track_title && taylor[i].year == year,
-    "The line should be the same song"
-  );
+//   var year = taylor[index].year;
+//   var track_title = taylor[index].track_title;
+//   var i = index - taylor[index].line + 1;
+//   console.assert(
+//     taylor[i].track_title == track_title && taylor[i].year == year,
+//     "The line should be the same song"
+//   );
 
-  while (taylor[i].track_title == track_title && taylor[i].year == year) {
-    s += taylor[i].line + ". " + taylor[i].lyric + "<br />";
-    i++;
-  }
+//   while (taylor[i].track_title == track_title && taylor[i].year == year) {
+//     s += taylor[i].line + ". " + taylor[i].lyric + "<br />";
+//     i++;
+//   }
 
-  var t = {...taylor[index]};
-  t.lyric = s;
-  console.log(s);
+//   var t = {...taylor[index]};
+//   t.lyric = s;
+//   console.log(s);
   var s = JSON.stringify(t);
   res.end(s);
 });
@@ -71,12 +62,12 @@ app.use(bodyParser.json());
 // content-type：application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// 简单路由
+// route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Kelly's server." });
 });
 
-// 设置监听端口
+// Set the listening port
 const PORT = process.env.PORT || 28282;
 app.listen(PORT, () => {
   console.log(`Kelly's lyric server runs on port ： ${PORT}.`);
